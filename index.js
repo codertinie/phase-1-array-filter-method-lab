eb3c76d39278bbb9fb2b861d44860622b2e04d0c
codertinie <martin.munyao@moringaschool.com>
2022-08-25 14:24:46 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(driverName, someString) {
+    return driverName.filter(
+      (matchingList) => matchingList.toLowerCase() === someString.toLowerCase()
+    );
+  }
+
+  function fuzzyMatch(driverName, anotherString){
+    return driverName.filter(
+        (matchingList) => matchingList.toLowerCase().indexOf(anotherString.toLowerCase()) === 0
+    );
+  }
+
+  function matchName(driverName, yetAnotherString){
+    return driverName.filter(
+        (collection) => collection.name === yetAnotherString
+    );
+  }
